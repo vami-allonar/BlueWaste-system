b05152d0c36e97e4ebab240302dc2a3698b9a32f
charlsrebaja <charlsrebaja09@gmail.com>
2026-07-06 20:31:10 +0800

    build(prisma): regenerate Prisma client artifacts for web

--- web/src/generated/prisma/edge.js
+++ web/src/generated/prisma/edge.js
@@ -155,7 +155,8 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null
+    "rootEnvPath": null,
+    "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
   "clientVersion": "6.19.3",
--- web/src/generated/prisma/index.js
+++ web/src/generated/prisma/index.js
@@ -156,7 +156,8 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null
+    "rootEnvPath": null,
+    "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
   "clientVersion": "6.19.3",
